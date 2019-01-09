2576f9c6f31c91e09f51b055e975b2962e5c22e3
Desi Mandasari <desimandasari1993@gmail.com>
2019-01-09 23:14:11 +0700

    add reactstrap and bootstrap

--- src/Register/index.js
+++ src/Register/index.js
@@ -1,51 +1,65 @@
 import React, { Component } from "react";
 
 class Register extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            name: "",
-            email: "",
-            password: "",
-            result: ""
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      result: ""
+    };
+  }
 
-    handleNameInput = event => {
-        this.setState({name: event.target.value})
-    }
+  handleNameInput = event => {
+    this.setState({ name: event.target.value });
+  };
 
-    handleEmailInput = event => {
-        this.setState({email: event.target.value})
-    }
+  handleEmailInput = event => {
+    this.setState({ email: event.target.value });
+  };
 
-    hanldePasswordInput = event => {
-        this.setState({password: event.target.value})
-    }
+  hanldePasswordInput = event => {
+    this.setState({ password: event.target.value });
+  };
 
-    handleRegisterSubmit = event => {
-        event.preventDefault()
-        const result = `${this.state.name} ${this.state.email} ${this.state.password}`
-        this.setState({ result: result })
-    }
+  handleRegisterSubmit = event => {
+    event.preventDefault();
+    const result = `${this.state.name} ${this.state.email} ${
+      this.state.password
+    }`;
+    this.setState({ result: result });
+  };
 
-    render() {
-        return (
-                <form onSubmit={this.handleRegisterSubmit}>
-                    <h3>Register</h3>
-                    <label htmlFor="name">Name:</label>
-                    <input type="name" placeholder="Type your name" onChange={this.handleNameInput} />
-                    <br />
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" placeholder="Type your email" onChange={this.handleEmailInput} />
-                    <br />
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" placeholder="Make it secure" onChange={this.handlePasswordInput} />
-                    <br />
-                    <input type="submit" value="Register" />
-                </form>
-        )
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleRegisterSubmit}>
+        <h3>Register</h3>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="name"
+          placeholder="Type your name"
+          onChange={this.handleNameInput}
+        />
+        <br />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          placeholder="Type your email"
+          onChange={this.handleEmailInput}
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          placeholder="Make it secure"
+          onChange={this.handlePasswordInput}
+        />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+    );
+  }
 }
- 
+
 export default Register;
